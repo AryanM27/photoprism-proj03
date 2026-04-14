aca192e12c9e2c3c14855bceb2c5e40feba6e5c8
Milind <milindkaushal123@gmail.com>
2026-04-13 23:31:14 -0400

    second training for mobnetv3

--- Training/src/aesthetic/train.py
+++ Training/src/aesthetic/train.py
@@ -169,7 +169,7 @@ def train_aesthetic_baseline(config_path: str) -> dict:
         config=config,
         image_size=config["model"]["image_size"],
         split="val",
-        start_index=0,
+        start_index=2500,
         max_records=2500,
     )
 
